182d2363a5330e267d28e61fad7aa2a55456adce
김용범 <subum12345@gmail.com>
2022-07-18 22:01:20 +0900

    전체 이미지

--- client/src/components/SearchImage/index.tsx
+++ client/src/components/SearchImage/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styled from "@emotion/styled/macro";
+
+const Image = styled.img`
+  width: 10rem;
+  height: 10rem;
+`;
 
 const SearchImage: React.FC = () => {
-  return <div>zz</div>;
+  return (
+    <div>
+      <Image src="https://cdn2.thedogapi.com/images/_q5yhP_X_.jpg" alt="사진" />
+    </div>
+  );
 };
 
 export default SearchImage;
