db7f6ea9ca9de956d77e0617fb06116bbca7994b
김용범 <subum12345@gmail.com>
2022-07-19 23:27:00 +0900

    router 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,41 @@
 // npm i @emotion/styled @emotion/react
-// npm i react-router-dom
+// npm i react-router-dom@5.3.3
+// npm i --save-dev @types/react-router-dom
+// npm i react-error-boundary
 // npm install react-icons --save
 // npm i axios
+// npm i redux react-redux redux-saga redux-devtools-extension redux-actions
+// npm i @types/react-redux @types/redux-actions -D
 
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+import styled from "@emotion/styled/macro";
+
 import Navbar from "./components/Nav";
 import Home from "./pages/Home";
+import Error from "./pages/Error";
+import NotFound from "./pages/NotFound";
 // import SignIn from "./pages/SignIn";
+
+const Base = styled.div`
+  padding-top: 8rem;
+`;
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/signin" element={<SignIn />} /> */}
-      </Routes>
-    </>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Base>
+            <Route exact path="/" component={Home} />
+            {/* <Route path="/signin" element={<SignIn />} /> */}
+            <Route component={NotFound} />
+          </Base>
+        </Switch>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
